161fe3ed8cd2ebb1a9bc3e89aea960f07d5d2236
Phil Howard <phil@gadgetoid.com>
2020-10-30 16:24:25 +0000

    Attempt to set CS channel for good luck

--- library/pmw3901/__init__.py
+++ library/pmw3901/__init__.py
@@ -25,6 +25,10 @@ class PMW3901():
     def __init__(self, spi_port=0, spi_cs_gpio=BG_CS_FRONT_BCM):
         self.spi_cs_gpio = spi_cs_gpio
         self.spi_dev = spidev.SpiDev()
+        try:
+            spi_cs = [8, 7].index(spi_cs_gpio)
+        except IndexError:
+            spi_cs = 0
         self.spi_dev.open(spi_port, spi_cs)
         self.spi_dev.max_speed_hz = 400000
         self.spi_dev.no_cs = True
