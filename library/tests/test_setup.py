49ea8c73c6bc6832787fb445ce3a68acb2aee233
Phil Howard <phil@gadgetoid.com>
2020-11-14 12:36:52 +0000

    Expand test coverage, fix bug

--- library/tests/test_setup.py
+++ library/tests/test_setup.py
@@ -1,26 +1,34 @@
-import sys
-import mock
+import pytest
 
 
-class SPIDevFakeDevice():
-    def __init__(self, bus=None, port=None):
-        self.regs = [0 for _ in range(512)]
-        self.regs[0x00] = 0x49  # Fake part ID
-        self.regs[0x01] = 0x00  # Fake revision ID
-
-    def open(self, bus, port):
-        pass
-
-    def xfer2(self, data):
-        self.ptr = data[0]
-        return [self.regs[self.ptr - 1 + x] for x in range(len(data))]
-
-
-def test_setup():
-    sys.modules['RPi'] = mock.Mock()
-    sys.modules['RPi.GPIO'] = mock.Mock()
-    sys.modules['spidev'] = mock.Mock()
-    sys.modules['spidev'].SpiDev = SPIDevFakeDevice
-    from pmw3901 import PMW3901
+def test_setup(GPIO, spidev, PMW3901):
     pmw3901 = PMW3901()
+
+    GPIO.setwarnings.assert_called_with(False)
+    GPIO.setmode.assert_called_with(GPIO.BCM)
+    GPIO.setup.assert_called_with(pmw3901.spi_cs_gpio, GPIO.OUT)
+
+
+def test_setup_invalid_gpio(GPIO, spidev, PMW3901):
+    pmw3901 = PMW3901(spi_cs_gpio=20)
+
+    assert spidev.SpiDev.open.called_with(0, 0)
+
     del pmw3901
+
+
+def test_setup_bg_front(GPIO, spidev, PMW3901):
+    PMW3901(spi_cs_gpio=7)
+    assert spidev.SpiDev.open.called_with(0, 1)
+
+
+def test_setup_bg_back(GPIO, spidev, PMW3901):
+    PMW3901(spi_cs_gpio=8)
+    assert spidev.SpiDev.open.called_with(0, 0)
+
+
+def test_setup_not_present(GPIO, spidev, PMW3901):
+    spidev._fakedev.regs[0x00] = 0x00
+
+    with pytest.raises(RuntimeError):
+        PMW3901()
