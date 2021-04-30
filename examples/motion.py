86f71390c8dd6d5a411ca417f88027ea3a096f98
Phil Howard <phil@gadgetoid.com>
2021-04-30 15:56:02 +0100

    Add --board arg to motion example

--- examples/motion.py
+++ examples/motion.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import time
 import argparse
-from pmw3901 import PMW3901, BG_CS_FRONT_BCM, BG_CS_BACK_BCM
+from pmw3901 import PMW3901, PAA5100, BG_CS_FRONT_BCM, BG_CS_BACK_BCM
 
 print("""motion.py - Detect flow/motion in front of the PMW3901 sensor.
 
@@ -9,6 +9,10 @@ Press Ctrl+C to exit!
 """)
 
 parser = argparse.ArgumentParser()
+parser.add_argument('--board', type=str,
+                    choices=['pmw3901', 'paa5100'],
+                    required=True,
+                    help='Breakout type.')
 parser.add_argument('--rotation', type=int,
                     default=0, choices=[0, 90, 180, 270],
                     help='Rotation of sensor in degrees.')
@@ -18,7 +22,10 @@ parser.add_argument('--spi-slot', type=str,
 
 args = parser.parse_args()
 
-flo = PMW3901(spi_port=0, spi_cs=1, spi_cs_gpio=BG_CS_FRONT_BCM if args.spi_slot == 'front' else BG_CS_BACK_BCM)
+# Pick the right class for the specified breakout
+SensorClass = PMW3901 if args.board == 'pmw3901' else PAA5100
+
+flo = SensorClass(spi_port=0, spi_cs=1, spi_cs_gpio=BG_CS_FRONT_BCM if args.spi_slot == 'front' else BG_CS_BACK_BCM)
 flo.set_rotation(args.rotation)
 
 tx = 0
