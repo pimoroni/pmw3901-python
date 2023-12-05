1cedf732328310fc333e403bb4b643d5a6c9a457
Phil Howard <phil@gadgetoid.com>
2023-12-05 14:58:37 +0000

    QA: Apply black suggestions.

--- examples/frame_capture.py
+++ examples/frame_capture.py
@@ -10,23 +10,16 @@ Press Ctrl+C to exit!
 """)
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--board', type=str,
-                    choices=['pmw3901', 'paa5100'],
-                    required=True,
-                    help='Breakout type.')
-parser.add_argument('--rotation', type=int,
-                    default=0, choices=[0, 90, 180, 270],
-                    help='Rotation of sensor in degrees.')
-parser.add_argument('--spi-slot', type=str,
-                    default='front', choices=['front', 'back'],
-                    help='Breakout Garden SPI slot.')
+parser.add_argument("--board", type=str, choices=["pmw3901", "paa5100"], required=True, help="Breakout type.")
+parser.add_argument("--rotation", type=int, default=0, choices=[0, 90, 180, 270], help="Rotation of sensor in degrees.")
+parser.add_argument("--spi-slot", type=str, default="front", choices=["front", "back"], help="Breakout Garden SPI slot.")
 
 args = parser.parse_args()
 
 # Pick the right class for the specified breakout
-SensorClass = PMW3901 if args.board == 'pmw3901' else PAA5100
+SensorClass = PMW3901 if args.board == "pmw3901" else PAA5100
 
-flo = SensorClass(spi_port=0, spi_cs=1, spi_cs_gpio=BG_CS_FRONT_BCM if args.spi_slot == 'front' else BG_CS_BACK_BCM)
+flo = SensorClass(spi_port=0, spi_cs=1, spi_cs_gpio=BG_CS_FRONT_BCM if args.spi_slot == "front" else BG_CS_BACK_BCM)
 flo.set_rotation(args.rotation)
 
 
