ff01ace661530fbbda193e8030836054d23f39c8
Phil Howard <phil@gadgetoid.com>
2024-05-07 15:57:27 +0100

    Update tests to handle GPIO-only SPI CS.

--- tests/test_paa5100.py
+++ tests/test_paa5100.py
@@ -9,22 +9,24 @@ def test_setup(gpiod, gpiodevice, spidev, PAA5100):
     gpiodevice.get_pin.assert_called_with(7, f"{paa5100._device_name}_cs", OUTL)
 
 
-def test_setup_invalid_gpio(gpiod, gpiodevice, spidev, PAA5100):
-    paa5100 = PAA5100(spi_cs_gpio=20)
+def test_setup_bg_front(gpiod, gpiodevice, spidev, PAA5100):
+    paa5100 = PAA5100(spi_cs_gpio=7)
 
     spidev.SpiDev().open.assert_called_with(0, 0)
 
-    del paa5100
+    OUTL = gpiod.LineSettings(direction=gpiod.Direction.OUTPUT, output_value=gpiod.Value.INACTIVE)
 
-
-def test_setup_bg_front(gpiod, gpiodevice, spidev, PAA5100):
-    PAA5100(spi_cs_gpio=7)
-    spidev.SpiDev().open.assert_called_with(0, 1)
+    gpiodevice.get_pin.assert_called_with(7, f"{paa5100._device_name}_cs", OUTL)
 
 
 def test_setup_bg_back(gpiod, gpiodevice, spidev, PAA5100):
-    PAA5100(spi_cs_gpio=8)
+    paa5100 = PAA5100(spi_cs_gpio=8)
+
     spidev.SpiDev().open.assert_called_with(0, 0)
+
+    OUTL = gpiod.LineSettings(direction=gpiod.Direction.OUTPUT, output_value=gpiod.Value.INACTIVE)
+
+    gpiodevice.get_pin.assert_called_with(8, f"{paa5100._device_name}_cs", OUTL)
 
 
 def test_setup_not_present(gpiod, gpiodevice, spidev, PAA5100):
--- tests/test_pmw3901.py
+++ tests/test_pmw3901.py
@@ -9,22 +9,24 @@ def test_setup(gpiod, gpiodevice, spidev, PMW3901):
     gpiodevice.get_pin.assert_called_with(7, f"{pmw3901._device_name}_cs", OUTL)
 
 
-def test_setup_invalid_gpio(gpiod, gpiodevice, spidev, PMW3901):
-    pmw3901 = PMW3901(spi_cs_gpio=20)
+def test_setup_bg_front(gpiod, gpiodevice, spidev, PMW3901):
+    pmw3901 = PMW3901(spi_cs_gpio=7)
 
     spidev.SpiDev().open.assert_called_with(0, 0)
 
-    del pmw3901
+    OUTL = gpiod.LineSettings(direction=gpiod.Direction.OUTPUT, output_value=gpiod.Value.INACTIVE)
 
-
-def test_setup_bg_front(gpiod, gpiodevice, spidev, PMW3901):
-    PMW3901(spi_cs_gpio=7)
-    spidev.SpiDev().open.assert_called_with(0, 1)
+    gpiodevice.get_pin.assert_called_with(7, f"{pmw3901._device_name}_cs", OUTL)
 
 
 def test_setup_bg_back(gpiod, gpiodevice, spidev, PMW3901):
-    PMW3901(spi_cs_gpio=8)
+    pmw3901 = PMW3901(spi_cs_gpio=8)
+
     spidev.SpiDev().open.assert_called_with(0, 0)
+
+    OUTL = gpiod.LineSettings(direction=gpiod.Direction.OUTPUT, output_value=gpiod.Value.INACTIVE)
+
+    gpiodevice.get_pin.assert_called_with(8, f"{pmw3901._device_name}_cs", OUTL)
 
 
 def test_setup_not_present(gpiod, gpiodevice, spidev, PMW3901):
